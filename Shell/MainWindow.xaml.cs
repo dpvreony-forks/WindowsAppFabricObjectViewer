6ca6ff8b548f1d49d53ad2bcbd90825810c03332
Don Pavlik <git@donpavlik.net>
2012-05-09 15:45:04 -0400

    Addition of initial solution structure.

---
+++ Shell/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Shell
+{
+	using System.Windows;
+
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			this.ContentRegion.Content = new Shell.HomeUserControl();
+		}
+	}
+}
